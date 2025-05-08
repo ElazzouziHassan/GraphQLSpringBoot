732f69293f6c0d7ab200be07db88a4ea3e28d432
Wizardy <wizardhassan99@gmail.com>
2025-05-08 15:05:50 +0100

    commit

---
+++ src/main/java/com/example/graphql/organiser/OrganiserService.java
@@ -0,0 +1,37 @@
+package com.example.graphql.organiser;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import com.example.graphql.exception.NotFoundException;
+
+import java.util.Collection;
+
+@Service
+@RequiredArgsConstructor
+public class OrganiserService {
+  private final OrganiserRepository organiserRepository;
+
+    public Collection<Organiser> list() {
+        return organiserRepository.findAll();
+    }
+
+    public Organiser findById(Long id) throws NotFoundException {
+        return organiserRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Organiser with id #" + id + " not found"));
+    }
+
+    public Organiser create(Organiser.OrganiserInput input) {
+        return organiserRepository.save(Organiser.builder()
+                .name(input.name()).build());
+    }
+
+    public Organiser modify(Organiser event, Organiser.OrganiserInput eventInput) {
+        event.setName(eventInput.name());
+        return organiserRepository.save(event);
+    }
+
+    public void delete(Organiser event) {
+        organiserRepository.delete(event);
+    }
+}
