67ca670296d8fb2e3ebd3768c06c50408f7d3379
Wizardy <wizardhassan99@gmail.com>
2025-05-08 15:06:04 +0100

    commit

---
+++ src/test/java/com/example/graphql_event/GraphqlEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graphql_event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
