2a38e1e2e2fca89ed701b3b05f87a7fec1d0b41a
Wizardy <wizardhassan99@gmail.com>
2025-05-08 15:05:17 +0100

    commit

---
+++ src/main/java/com/example/graphql/event/EventController.java
@@ -0,0 +1,45 @@
+package com.example.graphql.event;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import com.example.graphql.exception.NotFoundException;
+
+import java.util.Collection;
+
+@Controller
+@RequiredArgsConstructor
+public class EventController {
+  private final EventService eventService;
+
+    @QueryMapping
+    public Collection<Event> events() {
+        return eventService.list();
+    }
+
+    @QueryMapping
+    public Event eventById(@Argument Long id) throws Exception {
+        return eventService.findById(id);
+    }
+
+    @MutationMapping
+    public Event createEvent(@Argument Event.EventInput eventInput) throws NotFoundException {
+        return eventService.create(eventInput);
+    }
+
+    @MutationMapping
+    public Event modifyEvent(@Argument Long id, @Argument Event.EventInput eventInput) throws NotFoundException {
+        Event event = eventService.findById(id);
+        return eventService.modify(event, eventInput);
+    }
+
+    @MutationMapping
+    public Boolean deleteEvent(@Argument Long id) throws NotFoundException {
+        Event event = eventService.findById(id);
+        eventService.delete(event);
+        return true;
+    }
+}
