4bd216d5a0260c53e9be0ac8105c616780e984a6
Wizardy <wizardhassan99@gmail.com>
2025-05-08 15:05:13 +0100

    commit

---
+++ src/main/java/com/example/graphql/event/Event.java
@@ -0,0 +1,46 @@
+package com.example.graphql.event;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.example.graphql.organiser.Organiser;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Event {
+  @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private String type;
+    private String location;
+    private LocalDateTime time;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @ManyToOne
+    private Organiser organiser;
+
+    public record EventInput(
+            String name,
+            String description,
+            String type,
+            LocalDateTime time,
+            String location,
+            Long organiserId
+    ) {
+    }
+}
